333b2c8d3bfc583d45ced16267dc6a885df16807
ItsMeZeVas <veralegrojuansebastian@gmail.com>
2025-03-29 12:36:59 -0500

    Productos pagina principal

--- scriptSwiper.js
+++ scriptSwiper.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", function () {
     const nextButton = document.querySelector(".next");
 
     function getScrollAmount() {
-        const productWidth = 250; // 200px ancho + 10px margen a cada lado
+        const product = document.querySelector(".product");
+        if (!product) return 250; // Valor por defecto si no hay productos
+
+        const productWidth = product.clientWidth + 20; // Ancho real + margen
         const screenWidth = window.innerWidth;
 
         return screenWidth <= 768 ? productWidth : productWidth * 3;
@@ -16,10 +19,5 @@ document.addEventListener("DOMContentLoaded", function () {
 
     nextButton.addEventListener("click", function () {
         carousel.scrollBy({ left: getScrollAmount(), behavior: "smooth" });
-    });
-
-    // Asegurar que al cambiar el tamaño de la pantalla, se ajuste el scrollAmount dinámicamente
-    window.addEventListener("resize", function () {
-        getScrollAmount();
     });
 });
