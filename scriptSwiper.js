8f31e96212497e2e218838fa8b0e07919ccbca40
ItsMeZeVas <veralegrojuansebastian@gmail.com>
2025-03-29 14:19:54 -0500

    Pagina Principal Final

--- scriptSwiper.js
+++ scriptSwiper.js
@@ -3,21 +3,24 @@ document.addEventListener("DOMContentLoaded", function () {
     const prevButton = document.querySelector(".prev");
     const nextButton = document.querySelector(".next");
 
-    function getScrollAmount() {
-        const product = document.querySelector(".product");
-        if (!product) return 250; // Valor por defecto si no hay productos
+  function getScrollAmount() {
+    const product = document.querySelector(".product");
+    if (!product) return 0;
 
-        const productWidth = product.clientWidth + 20; // Ancho real + margen
-        const screenWidth = window.innerWidth;
+    const productWidth = product.clientWidth; // Usar el ancho real del producto
+    const screenWidth = window.innerWidth;
 
-        return screenWidth <= 768 ? productWidth : productWidth * 3;
-    }
+    return screenWidth <= 768 ? productWidth : productWidth * 3.1;
+}
 
     prevButton.addEventListener("click", function () {
         carousel.scrollBy({ left: -getScrollAmount(), behavior: "smooth" });
+        setTimeout(checkButtons, 500); // Verificar botones después del desplazamiento
     });
 
     nextButton.addEventListener("click", function () {
         carousel.scrollBy({ left: getScrollAmount(), behavior: "smooth" });
+        setTimeout(checkButtons, 500);
     });
+
 });
